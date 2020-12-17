cce36d6f45d739b4f53a8cb3a61222b5773e8c34
AlexZ-S <63847226+AlexZ-S@users.noreply.github.com>
2020-12-17 10:16:45 +0800

    2个新功能

---
+++ problems/eleUI弹窗回调函数.vue
@@ -0,0 +1,29 @@
+<script>
+    export default {
+        methods: {
+            upRisk() {
+                if (this.userInfo.status == 1) {
+                    this.fullscreenLoading = false,
+                        this.$confirm('您的风险测评已经超过一年, 点击确定重新测评?', '提示', {
+                            confirmButtonText: '确定',
+                            cancelButtonText: '取消',
+                            type: 'warning',
+                        }).then((action) => {
+                            // 执行区域
+                            if (action === 'confirm') {
+                                this.$router.replace({
+                                    path: '/account/risk_test'
+                                })
+                            }
+                        }).catch(() => {
+                            this.$message({
+                                type: 'info',
+                                message: '已取消重新测评'
+                            });
+                        });
+                }
+
+            },
+        }
+    }
+</script>
